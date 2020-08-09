f9ca63f786e990979ea301c29026c6ca4d4fa169
Eslam-Khder <eslamkhder91@gmail.com>
2020-08-09 17:16:11 +0200

    create UserRepository JPA

---
+++ src/main/java/com/spring/dao/UserRepository.java
@@ -0,0 +1,8 @@
+package com.spring.dao;
+
+import com.spring.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByUsername(String username);
+}
