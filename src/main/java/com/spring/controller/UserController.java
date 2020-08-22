5057728e160d63e511b46e061c383d10486a83e2
Eslam-Khder <eslamkhder91@gmail.com>
2020-08-22 09:52:02 +0200

    Validation if email Exist

--- src/main/java/com/spring/controller/UserController.java
+++ src/main/java/com/spring/controller/UserController.java
@@ -7,13 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.validation.Valid;
 import java.util.HashSet;
 import java.util.Set;
@@ -25,7 +26,7 @@ public class UserController {
     private PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserController(UserRepository userRepository,PasswordEncoder passwordEncoder) {
+    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
@@ -38,31 +39,34 @@ public class UserController {
 
     @PostMapping("/register")
     public String addUser(@Valid @ModelAttribute("user") User user,
-                          BindingResult theBindingResult) {
+                          BindingResult theBindingResult, Errors error, RedirectAttributes redirectAttributes) {
 
-        if(theBindingResult.hasErrors()) {
+        if (theBindingResult.hasErrors()) {
+            error.reject("existemail", "Email is Exist");
             return "view/usernewaccount";
-            //return "redirect:/signup";
         } else {
-            //Encode Password
-            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            if (userRepository.findByEmail(user.getEmail()) != null) {
+                redirectAttributes.addFlashAttribute("existemail","Email Exist");
+                return "redirect:/signup";
+            } else {
+                //Encode Password
+                user.setPassword(passwordEncoder.encode(user.getPassword()));
 
-            // set user active
-            user.setActive(1);
+                // set user active
+                user.setActive(1);
 
-            // Set Role Of User
-            Role role = new Role();
-            role.setId((long) 1);
-            Set<Role> ro = new HashSet<>();
-            ro.add(role);
-            user.setRoles(ro);
+                // Set Role Of User
+                Role role = new Role();
+                role.setId((long) 1);
+                Set<Role> ro = new HashSet<>();
+                ro.add(role);
+                user.setRoles(ro);
 
-            // save the user
-            userRepository.save(user);
+                // save the user
+                userRepository.save(user);
 
-            // use a redirect to prevent duplicate submissions
-            //return "redirect:/userlogin";
-            return "redirect:/login";
+                return "redirect:/login";
+            }
         }
 
     }
--- src/main/java/com/spring/dao/UserRepository.java
+++ src/main/java/com/spring/dao/UserRepository.java
@@ -2,6 +2,9 @@ package com.spring.dao;
 
 import com.spring.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     //User findByUsername(String username);
     User findByEmail(String email);
