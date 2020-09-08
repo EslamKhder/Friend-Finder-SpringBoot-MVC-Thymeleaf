246b4e1bdec0d2fe7ea80456986ec52ea26d8e6e
EslamKhder <eslamkhder81@gmail.com>
2020-09-08 18:59:41 +0200

    ShowFriends (isAnonymous)

--- src/main/java/com/spring/controller/HomeController.java
+++ src/main/java/com/spring/controller/HomeController.java
@@ -1,7 +1,9 @@
 package com.spring.controller;
 
 import com.spring.dao.CommentRepository;
+import com.spring.dao.FriendRepository;
 import com.spring.dao.PostRepository;
+import com.spring.dao.UserRepository;
 import com.spring.model.Post;
 import com.spring.model.User;
 import com.spring.util.UserData;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Collections;
 import java.util.List;
 
 @Controller
@@ -17,11 +20,15 @@ public class HomeController {
 
     private PostRepository postRepository;
     private CommentRepository commentRepository;
+    private FriendRepository friendRepository;
+    private UserRepository userRepository;
 
     @Autowired
-    public HomeController(PostRepository postRepository, CommentRepository commentRepository) {
+    public HomeController(PostRepository postRepository, CommentRepository commentRepository, FriendRepository friendRepository, UserRepository userRepository) {
         this.postRepository = postRepository;
         this.commentRepository = commentRepository;
+        this.friendRepository = friendRepository;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/")
@@ -33,12 +40,14 @@ public class HomeController {
     public String showMyPage(Model model) {
         model.addAttribute("posts",postRepository.findAll());
         if(UserData.isConnected()){
-            model.addAttribute("myposts",postRepository.findByUser(new User(UserData.userId())));
+            List<Post> posts = postRepository.findByUser(new User(UserData.userId()));
+            Collections.reverse(posts);
+            model.addAttribute("myposts",posts);
         }
         model.addAttribute("comments",commentRepository.findAll());
-
         model.addAttribute("imageprofile", UserData.userImage());
         model.addAttribute("post", new Post());
+        model.addAttribute("users",userRepository.findAll());
 
         return "/view/mainpage";
     }
--- src/main/java/com/spring/dao/FriendRepository.java
+++ src/main/java/com/spring/dao/FriendRepository.java
@@ -1,7 +1,11 @@
 package com.spring.dao;
 import com.spring.model.Friend;
+import com.spring.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface FriendRepository extends JpaRepository<Friend,Long> {
 
+    //public List<Friend> findByUser(User user);
 }
